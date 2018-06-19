9b46cd45157ba6e9e6ea1796b09c7da6fe6bed91
Eduardo Augusto Benigno da Silva <eaugusto@outlook.com>
2018-06-18 21:07:38 -0300

    Spring Boot Initial Commit

---
+++ takeiteasy-macroprocess/src/main/java/com/eaugusto/takeiteasy/macroprocess/MacroprocessApplication.java
@@ -0,0 +1,12 @@
+package com.eaugusto.takeiteasy.macroprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacroprocessApplication.class, args);
+	}
+}
